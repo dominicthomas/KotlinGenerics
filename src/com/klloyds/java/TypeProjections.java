724e0491aeb3edc53b2d16b86c0c9b7ae2eddc6e
Dominic Thomas <dominic.thomas@lloydsbanking.com>
2018-03-29 08:48:11 +0100

    Adding type projections

---
+++ src/com/klloyds/java/TypeProjections.java
@@ -0,0 +1,8 @@
+package com.klloyds.java;
+
+public class TypeProjections {
+
+    public static void main(String[] args) {
+        // TODO: Java equivalent
+    }
+}
---
+++ src/com/klloyds/kotlin/TypeProjections.kt
@@ -0,0 +1,7 @@
+package com.klloyds.kotlin
+
+fun main(args: Array<String>) {
+    // TODO: Type projection
+
+    // TODO: Start projection
+}
